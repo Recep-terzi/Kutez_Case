787b16af950047b6ee83a0901c348ef25b197fc9
Recep Terzi <recepterzi-67@hotmail.com>
2022-10-26 15:41:07 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1 className="kutez-title">The Company</h1>
+      <h2 className="kutez-next-title">Shipping Time Calculator</h2>
+      <div className="kutez-input">
+        <form>
+          <div className="order-date-input">
+            <input type="text" placeholder="Order Date" />
+          </div>
+          <div className="fabric-type-input">
+            <input type="text" placeholder="Fabric Type" />
+          </div>
+          <div className="quantity-input">
+            <input type="text" placeholder="Quantity" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
