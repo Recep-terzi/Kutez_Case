b73124a43afbc955b531ee571d2a5200180709e8
Recep Terzi <recepterzi-67@hotmail.com>
2022-10-26 21:00:15 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 function App() {
   return (
     <div className="app-container">
-      <h1 className="kutez-title">The Company</h1>
+      <h1 className="kutez-title">
+        <span>The Company</span>
+      </h1>
       <h2 className="kutez-next-title">Shipping Time Calculator</h2>
-      <div className="kutez-input">
-        <form>
+      <form>
+        <div className="kutez-input">
           <div className="order-date-input">
+            <i class="fa-regular fa-calendar"></i>
             <input type="text" placeholder="Order Date" />
           </div>
           <div className="fabric-type-input">
-            <input type="text" placeholder="Fabric Type" />
+            <select>
+              <option value="" disabled selected hidden>
+                Choose a drink
+              </option>
+              <option value="cotton">Cotton</option>
+              <option value="linen">Linen</option>
+            </select>
           </div>
           <div className="quantity-input">
-            <input type="text" placeholder="Quantity" />
+            <i class="fa-solid fa-circle-info"></i>
+            <input type="number" placeholder="Quantity" max="100" />
           </div>
-        </form>
+        </div>
+        <div className="kutez-button">
+          <button type="submit">Calculate</button>
+        </div>
+      </form>
+      <div className="kutez-info">
+        <h3>
+          Your Estimated Shipping Time Is <span>26 September 2023</span>
+        </h3>
       </div>
     </div>
   );
